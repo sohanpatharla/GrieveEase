8b7206189741435fcafa9c540200f9b06eebda57
Vinay2863 <vinay.sunkara2836@gmail.com>
2024-05-21 10:08:59 +0530

    user

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/UserModel");
+const User = require("../models/schema.model");
 
 async function registerUser(req, res) {
   const { email, password, username,name, mobileNumber, role } = req.body;
@@ -64,7 +64,7 @@ async function loginUser(req, res) {
 async function userDetails(req, res) {
     try {
         //const { id } = req.params.id
-        const user = await user.findById(req.body.id)
+        const user = await User.findById(req.body.id)
         if (user) {
             const { password, ...info } = user._doc;
             res.status(200).json(info);
